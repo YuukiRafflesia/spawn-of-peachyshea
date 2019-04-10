6bc89bd1232c2a954b2a7134cbe45980a21bb669
YuukiRafflesia <37911889+YuukiRafflesia@users.noreply.github.com>
2019-04-10 09:25:02 +0100

    Finally stopped commands working with literally any prefix... Or I hope atleast...

--- index.js
+++ index.js
@@ -26,18 +26,18 @@ bot.on('message', msg => {
 
     // *** RANDOM SHIT *** //
     
-    if (command === 'testo') {
+    if (msg.content === '&testo') {
         msg.channel.send(`Testingu testingu!`);
     }
 
-    if (command === 'dumbify') {
+    if (msg.content === '&dumbify') {
         let letterArr = args;
     }
 
     // *** FUN STUFF *** //
 
     /*
-    if (command === 'score') {
+    if (msg.content === '&score') {
         
         Math.floor(Math.random() * Math.floor(11));
     }
@@ -57,52 +57,52 @@ bot.on('message', msg => {
 
     // *** IMAGE POSTING *** //
 
-    if (command === 'bahaha') {
+    if (msg.content === '&bahaha') {
         msg.channel.send(`https://cdn.discordapp.com/attachments/412494829935919106/467904020275658762/Bahaha_v2.gif`);
     }
-    if (command === 'connordie') {
+    if (msg.content === '&connordie') {
         msg.channel.send(`https://cdn.discordapp.com/attachments/405533644250152962/505441929689956383/videotogif_2018.10.25_07.49.01.gif`);
     }
-    if (command === 'kumikonnor') {
+    if (msg.content === '&kumikonnor') {
         msg.channel.send(`https://cdn.discordapp.com/attachments/413449617401708555/457952177290215425/Kumikonnor.jpg`);
     }
-    if (command === 'penguinlove') {
+    if (msg.content === '&penguinlove') {
         msg.channel.send(`https://cdn.discordapp.com/attachments/416425531375091732/473460432921690128/Yuukin_do_it.gif`);
     }
-    if (command === 'ohio') {
+    if (msg.content === '&ohio') {
         msg.channel.send(`https://cdn.discordapp.com/attachments/412494829935919106/458308358491996160/good_morning.png`);
     }
-    if (command === 'patbob') {
+    if (msg.content === '&patbob') {
         msg.channel.send(`https://cdn.discordapp.com/attachments/469450295785553921/469458929223598120/What_monster_have_I_helped_create.gif`);
     }
-    if (command === 'peach') {
+    if (msg.content === '&peach') {
         msg.channel.send(`https://cdn.discordapp.com/attachments/405533644250152962/458418438704988181/IMG_20180519_184134_572-1.jpg`);
     }
-    if (command === 'smug') {
+    if (msg.content === '&smug') {
         msg.channel.send(`https://cdn.discordapp.com/attachments/412494829935919106/458961669012324362/at_this_point_im_just_trying_to_make_as_many_connor_memes_as_i_possibly_can.png`);
     }
-    if (command === 'suffer') {
+    if (msg.content === '&suffer') {
         msg.channel.send(`https://cdn.discordapp.com/attachments/446926093090291712/458863542653681664/image.jpg`);
     }
-    if (command === 'delicious') {
+    if (msg.content === '&delicious') {
         msg.channel.send(`https://cdn.discordapp.com/attachments/350298065741152286/502007678940217354/image0.gif`);
     }
-    if (command === 'tasty') {
+    if (msg.content === '&tasty') {
         msg.channel.send(`https://cdn.discordapp.com/attachments/350298065741152286/502007678940217354/image0.gif`);
     }
-    if (command === 'notworth') {
+    if (msg.content === '&notworth') {
         msg.channel.send(`https://cdn.discordapp.com/attachments/446298050541912065/457948301090881539/why_the_fuck_did_i_make_this.png`);
     }
-    if (command === 'thankspod') {
+    if (msg.content === '&thankspod') {
         msg.channel.send(`https://cdn.discordapp.com/attachments/405533644250152962/460647816575057950/videotogif_2018.06.24_23.26.33.gif`);
     }
-    if (command === 'welcome') {
+    if (msg.content === '&welcome') {
         msg.channel.send(`https://cdn.discordapp.com/attachments/446298050541912065/457948341989277706/what_the_fuck_am_i_doing_with_my_life.png`);
     }
-    if (command === 'wholesome') {
+    if (msg.content === '&wholesome') {
         msg.channel.send(`https://cdn.discordapp.com/attachments/412494829935919106/472863254318678034/not_sure_how_i_missed_that_the_first_time.gif`);
     }
-    if (command === 'thot') {
+    if (msg.content === '&thot') {
         msg.channel.send(`https://cdn.discordapp.com/attachments/405533644250152962/560851328935526424/image0.png`);
     }
 
